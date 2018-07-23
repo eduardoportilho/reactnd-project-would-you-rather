24acd57b538c050f34e0586ddda59b1607b5ef96
Eduardo Pilla Portilho <eduardo@portilho.com>
2018-07-23 15:01:19 +0200

    Persist redux state into localStorage

--- src/index.js
+++ src/index.js
@@ -5,25 +5,38 @@ import thunkMiddleware from 'redux-thunk'
 import { createLogger } from 'redux-logger'
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
+import { persistStore, persistReducer } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+import storage from 'redux-persist/lib/storage'
 import './style/index.css';
 import App from './components/App';
 import registerServiceWorker from './utils/registerServiceWorker';
 
-const loggerMiddleware = createLogger()
+// Persist redux state into localStorage
+const persistedReducer = persistReducer(
+  {
+    key: 'wouldyourather',
+    storage,
+  }, 
+  rootReducer,
+)
 
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   applyMiddleware(
     thunkMiddleware, // lets us dispatch() functions
-    loggerMiddleware // neat middleware that logs actions
-  )
+    createLogger(), // neat middleware that logs actions
+  ),
 )
+let persistor = persistStore(store)
 
 ReactDOM.render( 
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
-);
+)
 
-registerServiceWorker();
+registerServiceWorker()
